631e5117d1aba05545de973b397b9fdf300fa251
strideline <Travis@Traviss-MacBook-Pro.local>
2016-12-19 17:52:36 -0800

    did a lotta stuff

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-script.js
+var myForm = document.getElementById('myForm');
+var listedItems = document.getElementById('list');
+var toDo = document.getElementById('copy');
+// var list = document.getElementById('list');
+// var box = document.getElementById()
+
+myForm.addEventListener('submit', function(e) {
+  event.preventDefault();
+  var listItem = document.createElement('li');
+  listItem.innerHTML = toDo.value;
+  listedItems.appendChild(listItem);
+  event.target.toDo.value = '';
+});
+
+// list.addEventListener('click', function(e){
+//   list.
+
+// });
