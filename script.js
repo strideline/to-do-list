02aaff36128c3bf3491fc553b4b1d1205c2796c4
strideline <Travis@Traviss-MacBook-Pro.local>
2016-12-19 20:23:58 -0800

    changes

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 var myForm = document.getElementById('myForm');
 var listedItems = document.getElementById('list');
-var toDo = document.getElementById('copy');
+// var toDo = document.getElementById('copy');
 // var list = document.getElementById('list');
 // var box = document.getElementById()
 
 myForm.addEventListener('submit', function(e) {
   event.preventDefault();
+  
+  var toDo = event.target.toDo.value;
+
   var listItem = document.createElement('li');
-  listItem.innerHTML = toDo.value;
+  var deleteButton = document.createElement('button');
+  var clearDiv = document.createElement('div');
+
+  listItem.innerHTML = toDo;
+  // listedItems.appendChild(listItem);
+  // deleteButton.innerHTML = 'X';
+  deleteButton.className = 'button';
+  clearDiv.className = 'clearfix';
+
+  deleteButton.addEventListener('click', function(e){
+    event.target.parentNode.remove();
+  });
+
   listedItems.appendChild(listItem);
+  listedItems.appendChild(deleteButton);
+  listedItems.appendChild(clearDiv);
+
+  // toDo.appendChild(listedItems);
+
   event.target.toDo.value = '';
 });
 
-// list.addEventListener('click', function(e){
-//   list.
-
-// });
